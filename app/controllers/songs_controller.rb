b03656a5e2ac6942ae5a382026d1ffe17e31bc6a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 22:24:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,27 @@
 class SongsController < ApplicationController
 
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.find(params[:id])
+    if valid?
+      @song = Song.create(post_params)
+      redirect_to
+    else
+      render :new
+  end
+
+
+
+
 end
