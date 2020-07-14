6bc3e2351166a1d6b66539fbff4a8c9914a08433
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 22:18:38 +0000

    Automatically backed up by Learn

--- db/migrate/20200714221413_create_songs.rb
+++ db/migrate/20200714221413_create_songs.rb
@@ -2,9 +2,9 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
-      t.boolean :released
-      t.integer :release_year
       t.string :artist_name
+      t.integer :release_year
+      t.boolean :released
       t.string :genre
 
       t.timestamps
